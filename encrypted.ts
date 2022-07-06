4cd5fcb443d92887fc296efb40f7e0f4a5714a07
SaptarshiBagchi <55134979+SaptarshiBagchi@users.noreply.github.com>
2022-07-06 23:36:23 +0530

    Update encrypted.ts
    Completed Cipher

--- encrypted.ts
+++ encrypted.ts
@@ -1,6 +1,7 @@
 class MagicEncryption {
+    //The offset count
     private static secretNumber = 1
-    private static codeLength = 3
+    private static codeLength = 4
 
     private static chunkCreator(input: string[]) : string[][]{
         const arrays = []
@@ -12,11 +13,23 @@ class MagicEncryption {
     }   
 
     public static encode(input : string) : string {
-        return input.split('').map(character => {
+        return input.split('').map((character, index) => {
+            //step 1 : get the original unicode
             const originalUnicode = character.toLowerCase().charCodeAt(0)
+
+            //step 2: derive the left and right characters for distraction
             const leftChar = (originalUnicode)%26 - this.secretNumber
             const rightChar = (originalUnicode)%26 + this.secretNumber
-            return `${String.fromCharCode(leftChar+originalUnicode)}${character.toLowerCase()}${String.fromCharCode(rightChar + originalUnicode)}`
+
+            //step 4: prepare the position variable
+            // Get a random number 
+            const number = Math.floor(Math.random() * 10)
+            if(number % 2 === 0 || number === 0){
+                //if even keep the code in the middle
+                return `${number}${String.fromCharCode(leftChar+originalUnicode)}${character.toLowerCase()}${String.fromCharCode(rightChar + originalUnicode)}`
+            }
+            //if odd keep at the last
+            return `${number}${String.fromCharCode(leftChar+originalUnicode)}${String.fromCharCode(rightChar + originalUnicode)}${character.toLowerCase()}`
         }).join('')
     }
 
@@ -25,14 +38,17 @@ class MagicEncryption {
         const splittedInput = input.split('')
         const arrayOfChunks = this.chunkCreator(splittedInput)
         return arrayOfChunks.map(chunk => {
-            return chunk[1].toLowerCase()
+            if(parseInt(chunk[0]) % 2 ===0 || parseInt(chunk[0]) === 0) return chunk[2]
+            return chunk[3]
         }).join('')
     }
 }
 
 const encodedString = MagicEncryption.encode("Saptarshi Bagchi")
-const encodedStringToSearch = MagicEncryption.encode('bag')
-console.log({encodedString, encodedStringToSearch})
-const searchedString = new RegExp(encodedStringToSearch).test(encodedString)
-console.log(searchedString)
+const decodedString = MagicEncryption.decode(encodedString)
+console.log({encodedString, decodedString})
+// const encodedStringToSearch = MagicEncryption.encode('bag')
+// console.log({encodedString, encodedStringToSearch})
+// const searchedString = new RegExp(encodedStringToSearch).test(encodedString)
+// console.log(searchedString)
 
